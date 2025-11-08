86800c18963d8ec8b22dfb49399303ef4cefbf4a
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-11-08 21:36:47 +0100

    init

---
+++ src/Providers/ProductScraperServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Molitor\ProductScraper\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductScraperServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
